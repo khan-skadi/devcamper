d88d0159288401277607caa365691182654a3184
khan-skadi <kartalov.pt@gmail.com>
2020-05-26 17:37:35 +0200

    add protection and authorization to routes for user / admin/ publisher

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const {
   getBootcamps,
   getBootcamp,
@@ -7,34 +7,38 @@ const {
   deleteBootcamp,
   getBootcampsInRadius,
   bootcampPhotoUpload
-} = require("../controllers/bootcamps");
+} = require('../controllers/bootcamps');
 
-const Bootcamp = require("../models/Bootcamp");
-const advancedResults = require("../middleware/advancedResults");
+const Bootcamp = require('../models/Bootcamp');
+const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
-const courseRouter = require("./courses");
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const { protect, authorize } = require('../middleware/auth');
+
 // Re-route into other resource routers
-router.use("/:bootcampId/courses", courseRouter);
+router.use('/:bootcampId/courses', courseRouter);
 
 router
-  .route("/")
-  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
-  .post(createBootcamp);
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
-router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 // {{URL}}/api/v1/bootcamps?averageCost[lte]=10000&location.city=Boston
 
-router.route("/:id/photo").put(bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 // router.get('/', (req, res) => {
 //   res.status(200).json({ success: true, msg: 'Show all bootcamps' });
