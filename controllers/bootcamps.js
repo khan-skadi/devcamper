828b5321632a66c574d6f329bf97b7f6ae97bcc9
khan-skadi <kartalov.pt@gmail.com>
2020-05-26 18:18:15 +0200

    restrict permission for user and admin editing bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,8 @@
-const path = require("path");
-const ErrorResponse = require("../utils/errorResponse");
-const asyncHandler = require("../middleware/async");
-const geocoder = require("../utils/geocoder");
-const Bootcamp = require("../models/Bootcamp");
+const path = require('path');
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -30,7 +30,22 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-  // handle errors with trycatch
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamps
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already pubished a bootcamp`,
+        400
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({ success: true, data: bootcamp });
@@ -40,16 +55,28 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
   res.status(200).json({ success: true, data: bootcamp });
 });
@@ -63,6 +90,16 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to delete this bootcamp`,
+        401
+      )
     );
   }
 
@@ -88,7 +125,7 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
-    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
   });
 
   res
@@ -108,6 +145,16 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
   if (!req.files) {
     return next(new ErrorResponse(`Please upload a file`, 400));
   }
@@ -115,7 +162,7 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   const file = req.files.file;
 
   // Make sure the image is a photo
-  if (!file.mimetype.startsWith("image")) {
+  if (!file.mimetype.startsWith('image')) {
     return next(new ErrorResponse(`Please upload an image file`, 400));
   }
 
--- seeder.js
+++ seeder.js
@@ -1,30 +1,35 @@
-const fs = require("fs"); // file system module (included with node)
-const mongoose = require("mongoose");
-const colors = require("colors");
-const dotenv = require("dotenv");
+const fs = require('fs'); // file system module (included with node)
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 // Load models
-const Bootcamp = require("./models/Bootcamp");
-const Course = require("./models/Course");
+const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
 // Read JSON files
 const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
 
 const courses = JSON.parse(
-  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
+
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
 // Import into DB
@@ -32,8 +37,9 @@ const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
-    console.log("Data Imported..".green.inverse);
+    console.log('Data Imported..'.green.inverse);
     process.exit();
   } catch (err) {
     console.error(err);
@@ -45,17 +51,18 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
-    console.log("Data Destroyed..".red.inverse);
+    console.log('Data Destroyed..'.red.inverse);
     process.exit();
   } catch (err) {
     console.error(err);
   }
 };
 
-if (process.argv[2] === "-i") {
+if (process.argv[2] === '-i') {
   importData();
-} else if (process.argv[2] === "-d") {
+} else if (process.argv[2] === '-d') {
   deleteData();
 }
 
